9f63f2e17cea371dfbc7852f121ac0374ee497d0
Ratatouille <cshimmaincontact@gmail.com>
2023-11-30 02:44:45 -0500

    clerk working

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import GlobalStylesProvider from "./providers/GlobalStylesProvider";
 import ContextProvider from "./providers/ContextProvider";
-import { ClerkProvider } from "@clerk/nextjs";
+import { ClerkProvider, auth } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +18,8 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+
+  const {userId} = auth()
   return (
     <ClerkProvider>
       <html lang="en">
@@ -33,7 +35,8 @@ export default function RootLayout({
         <body className={inter.className}>
           <ContextProvider>
             <GlobalStylesProvider>
-              <Sidebar />
+              {userId && <Sidebar />}
+              
               <div className="w-full">{children}</div>
             </GlobalStylesProvider>
           </ContextProvider>
